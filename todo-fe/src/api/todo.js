3b8716b21d5dfc3614cb3a253c1be0150dbd42a2
xezzon <xezzon@outlook.com>
2025-09-10 20:43:25 +0800

    feat(frontend): Add `TodoService` client.

---
+++ todo-fe/src/api/todo.js
@@ -0,0 +1,17 @@
+import { createClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+
+// Import service definition that you want to connect to.
+import { TodoService } from "todo-proto/es/task_pb";
+
+// The transport defines what type of endpoint we're hitting.
+// In our example we'll be communicating with a Connect endpoint.
+// If your endpoint only supports gRPC-web, make sure to use
+// `createGrpcWebTransport` instead.
+const transport = createConnectTransport({
+  baseUrl: process.env.NEXT_PUBLIC_TODO_SERVICE,
+});
+
+// Here we make the client itself, combining the service
+// definition with the transport.
+export const todoClient = createClient(TodoService, transport);
